0c594df8461de777b86a2774d76f8917f0b59132
Abhijit Biswas <abhijit.biswas@lexmark.com>
2022-05-11 04:25:58 +0530

    json added and tested

--- script.js
+++ script.js
@@ -18,21 +18,19 @@ function addNewRow() {
     table.innerHTML+= 
                     '<tr><td><input type="text" class="ingredient"></td><td><input type="number" class="protein"></td><td><input type="text" class="carbohydrate"></td><td><input type="text" class="fat"></td><td><input type="text" class="energy"></td></tr>';
                 
-                // let ingredientName = document.getElementById('ingredientName').value;
-                ingredients[inserts] = document.getElementById('ingredientName').value;
-                
-                // let proteinAmount = document.getElementById('proteinAmount').value;
-                proteinAmounts[inserts] = document.getElementById('proteinAmount').value;
-                carbohydrateAmounts[inserts] = document.getElementById('carbohydrateAmount').value;
-                fatAmounts[inserts] = document.getElementById('fatAmount').value;
-                energyAmounts[inserts] = document.getElementById('energyAmount').value;
+    ingredients[inserts] = document.getElementById('ingredientName').value;
+    proteinAmounts[inserts] = document.getElementById('proteinAmount').value;
+    carbohydrateAmounts[inserts] = document.getElementById('carbohydrateAmount').value;
+    fatAmounts[inserts] = document.getElementById('fatAmount').value;
+    energyAmounts[inserts] = document.getElementById('energyAmount').value;
 
-                ingredient[inserts].value = ingredients[inserts];
-                protein[inserts].value = proteinAmounts[inserts];
-                carbohydrate[inserts].value = carbohydrateAmounts[inserts];
-                fat[inserts].value = fatAmounts[inserts];
-                energy[inserts].value = energyAmounts[inserts];
-                inserts++;
+    ingredient[inserts].value = ingredients[inserts];
+    protein[inserts].value = proteinAmounts[inserts];
+    carbohydrate[inserts].value = carbohydrateAmounts[inserts];
+    fat[inserts].value = fatAmounts[inserts];
+    energy[inserts].value = energyAmounts[inserts];
+
+    inserts++;
 }
 
 form.addEventListener("submit", (e) => {
@@ -46,3 +44,14 @@ form.addEventListener("submit", (e) => {
         energy[i].value = energyAmounts[i];
     }
 });
+
+fetch('values.json')
+  .then(response => response.json())
+  .then( data => appendData(data))
+  .catch( err => console.log(err));
+
+function appendData(data) {
+    // let ing = document.getElementById('ingredientName').value;
+    console.log(data);
+    document.getElementById('proteinAmount').value = data.testIngredient2.protein;
+}
